7e48cff2dc67d98363cf1e703109c7086e041c91
Patipat C <pcbimon@gmail.com>
2019-03-27 16:57:05 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import './app_screens/first_screen.dart';
+
+void main() =>
+  runApp(new MyFlutterApp());
+
+class MyFlutterApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "My Flutter App",
+        home: Scaffold(
+          appBar: AppBar(
+              title: Text("My First App Screen")
+          ),
+          body: FirstScreen()
+        )
+
+    );
+  }
+
+}
